72cbc80b689db3c9c208770aa01fd1e964ea1995
FredVigoria <231820@unsaac.edu.pe>
2024-07-27 10:07:13 -0500

    Add files via upload

--- Problema2.py
+++ Problema2.py
@@ -5,13 +5,13 @@ import MiLibreria
 # Definimos nuestra funcion
 def Inclusion(X, Y):
     if MiLibreria.EstaIncluido(X, Y) and MiLibreria.EstaIncluido(Y, X):
-        return "Ambos arreglos son iguales"
+        return 0
     elif MiLibreria.EstaIncluido(X, Y):
-        return "X esta incluido en Y"
+        return 1
     elif MiLibreria.EstaIncluido(Y, X):
-        return "Y esta incluido en X"
+        return -1
     else:
-        return "No hay inclusion"
+        return None
 
 # ------------- Ejemplo de uso ------------------
 # Leer los datos
@@ -23,4 +23,4 @@ Y = MiLibreria.LeerLista("Insertar Y: ", M)
 # Mostrar los resultados
 print("Conjunto X: ", X)
 print("Conjunto Y: ", Y)
-print(Inclusion(X, Y))  
+print("La relacion de inclusion de X e Y es: ",Inclusion(X, Y))
